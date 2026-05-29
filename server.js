53398a10adb4391a0b7f092125a354d51c4f7ad1
David Kim <dak2367@mavs.uta.edu>
2026-05-28 22:03:30 -0500

    added railway

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server running')
 })
 // BOILERPLATE //
 
